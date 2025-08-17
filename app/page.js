7324c6b82c5ec71b4178568e1df0dafea9a0f4d3
yutanakao <yutanakao0717@gmail.com>
2025-08-17 20:00:24 +0900

    fix

--- app/page.js
+++ app/page.js
@@ -4,10 +4,14 @@ import { FaInstagram } from 'react-icons/fa';
 export default function Home() {
   return (
     <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <div className="grid gap-5">
-        <h1 className="text-4xl font-bold">ページを作成中です</h1>
+      <div className="grid gap-8">
+        <div className="grid gap-6">
+          <h1 className="text-5xl font-bold">王朱海の招福占い</h1>
+          <h2 className="text-2xl font-bold pl-1">現在ページを作成中です</h2>
+        </div>
 
         <div className="grid gap-3 pl-2">
+          <h3 className="text-xl font-bold mb-2">SNSで更新中</h3>
           <a
             href="https://x.com/ouakemi"
             target="_blank"
